ec79de47047a97ff429d89083f177c90dd11d2c4
smitiscoder <smitnerurkar4@gmail.com>
2025-05-20 01:27:50 +0530

    screenshots done

--- src/components/lib/screenshots.js
+++ src/components/lib/screenshots.js
@@ -1,37 +1,37 @@
 export const screenshots = [
   {
     id: 1,
-    src: "https://i.postimg.cc/CMs3yg98/IMG-20250519-214757.jpg",
+    src: "https://i.postimg.cc/x89dn74R/IMG-20250519-214757.jpg",
     alt: "Home screen of the OutFlow",
     caption: "Home screen of the OutFlow",
   },
   {
     id: 2,
-    src: "https://i.postimg.cc/sX9NRYHz/IMG-20250519-214941.jpg",
+    src: "https://i.postimg.cc/BQYvfmcT/IMG-20250519-214941.jpg",
     alt: "Expense category selection",
     caption: "Expense category selection",
   },
   {
     id: 3,
-    src: "https://i.postimg.cc/9MGvQ5Mv/IMG-20250519-214650.jpg",
+    src: "https://i.postimg.cc/0y4rDVzk/IMG-20250519-214650.jpg",
     alt: "Custom keyboard for data entry",
     caption: "Custom keyboard for data entry",
   },
   {
     id: 4,
-    src: "https://i.postimg.cc/0yF3G2KT/IMG-20250519-214854.jpg",
+    src: "https://i.postimg.cc/9XBMJmDN/IMG-20250519-214854.jpg",
     alt: "Financial report overview",
     caption: "Financial report overview",
   },
   {
     id: 5,
-    src: "https://i.postimg.cc/MKdJBSDw/IMG-20250519-214614.jpg",
+    src: "https://i.postimg.cc/d07kxzbZ/IMG-20250519-214614.jpg",
     alt: "Advance Search functionality",
     caption: "Advance Search functionality",
   },
   {
     id: 6,
-    src: "https://i.postimg.cc/qB2kzSSB/IMG-20250519-214557.jpg",
+    src: "https://i.postimg.cc/522FwHp3/IMG-20250519-214557.jpg",
     alt: "User profile and settings",
     caption: "User profile and settings",
   },
