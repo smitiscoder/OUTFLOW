b5af6edd59d6df52f565ad53cb9e0f9d4e45bc8d
smitiscoder <smitnerurkar4@gmail.com>
2025-05-20 00:59:59 +0530

    4

--- src/components/lib/screenshots.js
+++ src/components/lib/screenshots.js
@@ -1,37 +1,37 @@
 export const screenshots = [
   {
     id: 1,
-    src: "/images/Home.jpg",
+    src: "https://i.postimg.cc/CMs3yg98/IMG-20250519-214757.jpg",
     alt: "Home screen of the OutFlow",
     caption: "Home screen of the OutFlow",
   },
   {
     id: 2,
-    src: "/images/ExpenseCategory.jpg",
+    src: "https://i.postimg.cc/sX9NRYHz/IMG-20250519-214941.jpg",
     alt: "Expense category selection",
     caption: "Expense category selection",
   },
   {
     id: 3,
-    src: "/images/Keyboard.jpg",
+    src: "https://i.postimg.cc/9MGvQ5Mv/IMG-20250519-214650.jpg",
     alt: "Custom keyboard for data entry",
     caption: "Custom keyboard for data entry",
   },
   {
     id: 4,
-    src: "/images/Report.jpg",
+    src: "https://i.postimg.cc/0yF3G2KT/IMG-20250519-214854.jpg",
     alt: "Financial report overview",
     caption: "Financial report overview",
   },
   {
     id: 5,
-    src: "/images/Search.jpg",
+    src: "https://i.postimg.cc/MKdJBSDw/IMG-20250519-214614.jpg",
     alt: "Advance Search functionality",
     caption: "Advance Search functionality",
   },
   {
     id: 6,
-    src: "/images/Profile.jpg",
+    src: "https://i.postimg.cc/qB2kzSSB/IMG-20250519-214557.jpg",
     alt: "User profile and settings",
     caption: "User profile and settings",
   },
