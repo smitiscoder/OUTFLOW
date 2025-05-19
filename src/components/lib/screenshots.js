db9e71140a76c4bd6223706cb7873ed6b939d9cd
smitiscoder <smitnerurkar4@gmail.com>
2025-05-20 00:44:34 +0530

    2

--- src/components/lib/screenshots.js
+++ src/components/lib/screenshots.js
@@ -1,38 +1,39 @@
 export const screenshots = [
   {
     id: 1,
-    src: "/images/Home.jpg",
+    src: `${process.env.PUBLIC_URL}/images/Home.jpg`,
     alt: "Home screen of the OutFlow",
     caption: "Home screen of the OutFlow",
   },
   {
     id: 2,
-    src: "/images/ExpenseCategory.jpg",
+    src: `${process.env.PUBLIC_URL}/images/ExpenseCategory.jpg`,
     alt: "Expense category selection",
     caption: "Expense category selection",
   },
   {
     id: 3,
-    src: "/images/Keyboard.jpg",
+    src: `${process.env.PUBLIC_URL}/images/Keyboard.jpg`,
     alt: "Custom keyboard for data entry",
     caption: "Custom keyboard for data entry",
   },
   {
     id: 4,
-    src: "/images/Report.jpg",
+    src: `${process.env.PUBLIC_URL}/images/Report.jpg`,
     alt: "Financial report overview",
     caption: "Financial report overview",
   },
   {
     id: 5,
-    src: "/images/Search.jpg",
+    src: `${process.env.PUBLIC_URL}/images/Search.jpg`,
     alt: "Advance Search functionality",
     caption: "Advance Search functionality",
   },
   {
     id: 6,
-    src: "/images/Profile.jpg",
+    src: `${process.env.PUBLIC_URL}/images/Profile.jpg`,
     alt: "User profile and settings",
     caption: "User profile and settings",
   },
 ];
+
